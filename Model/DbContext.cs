74f027f1200d7ddc43383359461bd6fc37ac2d5e
Mobiasson <mikaeltobiasson@hotmail.com>
2025-12-10 08:48:11 +0100

    Clean up: stop tracking bin/obj/.vs folders

--- Model/DbContext.cs
+++ Model/DbContext.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System.Configuration;
 
 namespace Insulter.Model;
 
@@ -13,5 +12,5 @@ public partial class InsultContext : DbContext {
     public virtual DbSet<Insult> Insults { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseSqlServer("Data Source=localhost;Database=insulter;Integrated Security=True;TrustServerCertificate=True;");
+        => optionsBuilder.UseSqlServer("Data Source=localhost;Database=Insulter;Integrated Security=True;TrustServerCertificate=True;");
 }
