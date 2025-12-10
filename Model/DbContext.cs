b51edf8810af3d912977429843ea6397daeccd12
Mobiasson <mikaeltobiasson@hotmail.com>
2025-12-10 08:33:07 +0100

    Added DBconnection and .gitignore

---
+++ Model/DbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+
+namespace Insulter.Model;
+
+public partial class InsultContext : DbContext {
+    public InsultContext() {
+    }
+
+    public InsultContext(DbContextOptions<InsultContext> options) : base(options) {
+    }
+
+    public virtual DbSet<Insult> Insults { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Data Source=localhost;Database=insulter;Integrated Security=True;TrustServerCertificate=True;");
+}
---
+++ Model/Insult.cs
@@ -0,0 +1,7 @@
+﻿namespace Insulter.Model;
+public class Insult {
+    public int? Id { get; set; }
+    public string? Text { get; set; }
+
+}
+
