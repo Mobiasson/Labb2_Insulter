ddd1c958eae995bee595ef60401504be003256c2
Mobiasson <mikaeltobiasson@hotmail.com>
2026-01-15 12:55:35 +0100

    Added functionality to delete users from database

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -5,15 +5,23 @@ using Insulter.Model;
 using Insulter.Dialog;
 using Insulter.Services;
 using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace Insulter;
 
 public partial class MainWindow : INotifyPropertyChanged {
     private string? _insult = "Click 'Insult Me' to get roasted!";
+    private string? _user;
     public string LoadingMessage { get; set; } = "Please wait...";
     public string? Insult {
         get => _insult;
         set { _insult = value; OnPropertyChanged(); }
+    }
+    public string? User {
+        get => _user;
+        set {
+            _user = value; OnPropertyChanged();
+        }
     }
 
     private bool _isLoading;
@@ -45,10 +53,9 @@ public partial class MainWindow : INotifyPropertyChanged {
         finally {
             IsLoading = false;
         }
-
     }
 
-    private async void ClearDatabase_Click(object sender, RoutedEventArgs e) {
+    private async void ClearInsults_Click(object sender, RoutedEventArgs e) {
         try {
             if(MessageBox.Show("Are you sure you wnat to delete all insults in the database?",
                     "Confirmation",
@@ -69,8 +76,27 @@ public partial class MainWindow : INotifyPropertyChanged {
             IsLoading = false;
         }
     }
+    private async void ClearUsers_Click(object sender, RoutedEventArgs e) {
+        try {
+            if(MessageBox.Show("Are you sure you want to clear all users from the database?", "Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes) {
+                IsLoading = true;
+                LoadingMessage = "Deleting users from database...";
+                await using var db = new InsultContext();
+                var deleted = await db.Users.ExecuteDeleteAsync();
+                User = deleted > 0 ? $"Deleted {User} users from the database" : "No users in database to delete";
+                MessageBox.Show(User, "Deleted all users", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+        catch(System.Exception ex) {
+            MessageBox.Show(ex.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+        finally {
+            IsLoading = false;
+        }
+    }
 
     public event PropertyChangedEventHandler? PropertyChanged;
     private void OnPropertyChanged([CallerMemberName] string? name = null)
         => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
 }
